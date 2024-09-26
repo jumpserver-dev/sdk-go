7dde41c2ec607c58d8331ec2f79f9e2381ab449c
Eric <xplzv@126.com>
2024-09-26 14:56:36 +0800

    perf: modify log

--- logger/log.go
+++ logger/log.go
@@ -13,48 +13,48 @@ type StdLogger struct {
 	Logf  Logf
 }
 
-var logger = StdLogger{
+var stdLogger = StdLogger{
 	Level: slog.LevelInfo,
 	Logf:  log.Printf,
 }
 
 func SetUp(level slog.Level, logf Logf) {
-	logger.Level = level
-	logger.Logf = logf
-}
-
-func Errorf(format string, args ...any) {
-	if logger.Level > slog.LevelError {
-		return
-	}
-	logger.Logf(format, args...)
+	stdLogger.Level = level
+	stdLogger.Logf = logf
 }
 
 func Warnf(format string, args ...any) {
-	if logger.Level > slog.LevelWarn {
+	if stdLogger.Level > slog.LevelWarn {
 		return
 	}
-	logger.Logf(format, args...)
+	stdLogger.Logf(format, args...)
 }
 
 func Infof(format string, args ...any) {
-	if logger.Level > slog.LevelInfo {
+	if stdLogger.Level > slog.LevelInfo {
 		return
 	}
-	logger.Logf(format, args...)
+	stdLogger.Logf(format, args...)
 }
 
 func Debugf(format string, args ...any) {
-	if logger.Level > slog.LevelDebug {
+	if stdLogger.Level > slog.LevelDebug {
 		return
 	}
-	logger.Logf(format, args...)
+	stdLogger.Logf(format, args...)
 }
 
 func Error(args ...any) {
-	if logger.Level > slog.LevelError {
+	if stdLogger.Level > slog.LevelError {
 		return
 	}
 	fmtStr := strings.Repeat("%v ", len(args))
-	logger.Logf(fmtStr, args...)
+	stdLogger.Logf(fmtStr, args...)
+}
+
+func Errorf(format string, args ...any) {
+	if stdLogger.Level > slog.LevelError {
+		return
+	}
+	stdLogger.Logf(format, args...)
 }

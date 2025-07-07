98c9d10712c9f836bb72f2839112ee36d86d8fa8
Eric <xplzv@126.com>
2025-07-07 18:59:10 +0800

    perf: add miss file

---
+++ service/jms_db_ports.go
@@ -0,0 +1,20 @@
+package service
+
+import (
+	"strconv"
+
+	"github.com/jumpserver-dev/sdk-go/model"
+)
+
+func (s *JMService) GetListenPorts() (ports []int32, err error) {
+	_, err = s.authClient.Get(DBListenPortsURL, &ports)
+	return
+}
+
+func (s *JMService) GetAssetByPort(port int32) (app model.Asset, err error) {
+	data := map[string]string{
+		"port": strconv.Itoa(int(port)),
+	}
+	_, err = s.authClient.Get(DBPortInfoURL, &app, data)
+	return
+}

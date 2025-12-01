80d314122472cfe00a7823fbfe14206007054f04
Eric <xplzv@126.com>
2025-12-01 15:42:20 +0800

    perf: add user roles

--- model/user.go
+++ model/user.go
@@ -16,6 +16,9 @@ type User struct {
 	Language         string `json:"lang"`
 	HasPublicKeys    bool   `json:"has_public_keys"`
 	IsServiceAccount bool   `json:"is_service_account"`
+
+	OrgRoles    []OrgRole    `json:"org_roles"`
+	SystemRoles []SystemRole `json:"system_roles"`
 }
 
 type MiniUser struct {
@@ -33,4 +36,15 @@ type UserKokoPreference struct {
 }
 type KokoBasic struct {
 	ThemeName string `json:"terminal_theme_name"`
+}
+
+type OrgRole struct {
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	DisplayName string `json:"display_name"`
+}
+
+type SystemRole struct {
+	ID          string `json:"id"`
+	DisplayName string `json:"display_name"`
 }
